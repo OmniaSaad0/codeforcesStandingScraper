0c42847b0220be7f5bd5ba36dab05522b1a3a2c0
0__0 <77336471+OmniaSaad0@users.noreply.github.com>
2023-05-04 00:45:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
-from itertools import zip_longest
 import pandas as pd
+
+
 
 
 def standing(url) :
@@ -16,36 +16,54 @@ def standing(url) :
         row_data = []
         cnt = 0
         for cell in row.find_all('td'):
-            if cnt == 3:
+            if cnt == 4:
                 break
             cnt += 1
             row_data.append(cell.text.strip())
         table_data.append(row_data)
 
-    df = pd.DataFrame(table_data[1:], columns=['Rank', 'Handle', '# of solved problems'])
+    df = pd.DataFrame(table_data[1:], columns=['Rank', 'Handle', '# of solved problems','Penalty'])
 
     return df
 
-def merge(con1 , con2):
-    total = pd.merge(con1, con2, on='Handle', how='outer', suffixes=('_con1', '_con2'))
+
+def merge(contests):
+    total = contests[0]
+    for i in range(1, len(contests)):
+        suffix1 = str(i)
+        suffix2 = str(i + 1)
+        total = pd.merge(total, contests[i], on='Handle',how= 'outer', suffixes=('', f'_{suffix2}'))
+
     total.fillna(0, inplace=True)
-    total['total solved problems'] = total['# of solved problems_con1'].astype('int') + total['# of solved problems_con2'].astype('int')
-    total.sort_values('total solved problems', inplace=True, ignore_index=True, ascending=False)
-    total = total.drop(columns=['Rank_con1', 'Rank_con2'])
+
+    total['total solved problems'] = total.filter(like='# of solved problems').sum(axis=1)
+    total['total penalty'] = total.filter(like='Penalty').sum(axis=1)
+
+    delete_column = total.filter(like= 'Penalty').columns.tolist()
+    total = total.drop(columns=delete_column)
+
+    delete_column = total.filter(like= 'Rank').columns.tolist()
+    total = total.drop(columns=delete_column)
+
+    total.sort_values(by = ['total solved problems','total penalty'], inplace=True, ignore_index=True , ascending= [False,True])
+
     total.to_csv("total.csv", index=False)
     print("Done...")
 
 
+contest_num = int(input('Enter Number of Contests you want to merge their standing, you should enter at least 2 standing to merge  : '))
+while contest_num < 2 :
+    contest_num = int(input('Enter Number of Contests you want to merge their standing, you should enter at least 2 standing to merge  : '))
 
-url = input("Enter Contest URL 1 : ")
-df1 = standing(url)
-url = input("Enter Contest URL 2 : ")
-df2 = standing(url)
-df1['# of solved problems'] = pd.to_numeric(df1['# of solved problems'], errors='coerce')
-df2['# of solved problems'] = pd.to_numeric(df2['# of solved problems'], errors='coerce')
+df = []
+for i in range(1, contest_num + 1):
+    url = input(f"Enter Contest URL {i} : ")
+    df.append(standing(url))
+    df[i-1]['# of solved problems'] = pd.to_numeric(df[i-1]['# of solved problems'], errors='coerce')
+    df[i - 1]['Penalty'] = pd.to_numeric(df[i - 1]['Penalty'], errors='coerce')
 
 
-merge(df1,df2)
+merge(df)
 
 
 
